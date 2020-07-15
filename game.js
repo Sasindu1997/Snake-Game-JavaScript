7f633d4ff20be885749d03fc5a8b21096c7af17c
Sasindu1997 <sasinduusgalhewa@gmail.com>
2020-07-15 22:29:25 +0530

    set up startup positions

--- game.js
+++ game.js
@@ -1,12 +1,23 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+
 let lastRenderTime = 0
-const SNAKE_SPEED = 2
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
     window.requestAnimationFrame(main)
     const secondSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if(secondSinceLastRender < 1 / SNAKE_SPEED) return
+    if (secondSinceLastRender < 1 / SNAKE_SPEED) return
 
-    console.log('Render')
     lastRenderTime = currentTime
+
+    update()
+    draw()
 }
 window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+}
+function draw() {
+    drawSnake(gameBoard)
+}
