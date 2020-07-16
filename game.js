0dfca74f8f62df1f56a42b3a62e00982fb854c35
Sasindu1997 <sasinduusgalhewa@gmail.com>
2020-07-16 21:14:28 +0530

    set game over

--- game.js
+++ game.js
@@ -1,10 +1,16 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
 
 let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
+    if(gameOver){
+        return alert('you loose')
+    }
+    
     window.requestAnimationFrame(main)
     const secondSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondSinceLastRender < 1 / SNAKE_SPEED) return
@@ -19,9 +25,13 @@ window.requestAnimationFrame(main)
 function update() {
     updateSnake()
     updateFood()
+    checkDeath()
 }
 function draw() {
     gameBoard.innerHTML = ''
     drawSnake(gameBoard)
     drawFood(gameBoard)
+}
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
